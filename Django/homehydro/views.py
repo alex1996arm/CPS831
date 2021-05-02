0597fc655ac9d5b5b679e9aff32f9ae2b810f736
Alexandre Roberge-Milanese <alex1996arm@gmail.com>
2021-05-02 14:54:16 -0400

    accessing the influxDB API

--- Django/homehydro/views.py
+++ Django/homehydro/views.py
@@ -1,10 +1,25 @@
 from django.http import HttpResponse
 from django.template import loader
+from influxdb_client import InfluxDBClient
+
+
 
 def index(request):
+
+    query = 'from(bucket: "mybucket")\
+    |> range(start: v.timeRangeStart, stop: v.timeRangeStop)\
+    |> filter(fn: (r) => r["_measurement"] == "mqtt_consumer")\
+    |> filter(fn: (r) => r["_field"] == "DS18B20-1_Temperature" or r["_field"] == "DS18B20-2_Temperature")\
+    |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)\
+    |> yield(name: "mean")'
+    client = InfluxDBClient(url="http://0.0.0.0:8086", token='mytoken', org='fff7232ade399db6')
+    query_api = client.query_api()
+    result = client.query_api().query(org='fff7232ade399db6', query=query)
     template = loader.get_template('index.html')
     graph=0
     context = {
         'graph': graph,
     }
     return HttpResponse(template.render(context,request))
+
+
