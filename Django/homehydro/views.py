8d00c01eda545019a631941b822b41e162b3ef6e
Alexandre Roberge-Milanese <alex1996arm@gmail.com>
2021-05-03 02:13:02 -0400

    latest commit

--- Django/homehydro/views.py
+++ Django/homehydro/views.py
@@ -12,9 +12,9 @@ def index(request):
     |> filter(fn: (r) => r["_field"] == "DS18B20-1_Temperature" or r["_field"] == "DS18B20-2_Temperature")\
     |> aggregateWindow(every: v.windowPeriod, fn: mean, createEmpty: false)\
     |> yield(name: "mean")'
-    client = InfluxDBClient(url="http://0.0.0.0:8086", token='mytoken', org='fff7232ade399db6')
-    query_api = client.query_api()
-    result = client.query_api().query(org='fff7232ade399db6', query=query)
+    client = InfluxDBClient(url="http://0.0.0.0:8086", token='8ZRr55aK71lehamceaFenj6LHfV2IW8shEdapcEJS_h954--rgnd4-ydZgGyG_ghnU0mw6Y9573jLRZ4k0MoFQ==', org='myorg')
+    #query_api = client.query_api()
+    #result = client.query_api().query(org='fff7232ade399db6', query=query)
     template = loader.get_template('index.html')
     graph=0
     context = {
